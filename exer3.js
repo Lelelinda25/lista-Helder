94eb09d31da0340591368a6b98d77fb91992e17f
Lelelinda25 <leticia.souza10@estudante.ifms.edu.br>
2026-06-01 13:20:31 -0400

    exer3

---
+++ exer3.js
@@ -0,0 +1,11 @@
+class Produto {
+    constructor(nome, preco, quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        Produto.totalProdutos++;
+    }
+
+    
+
+}
