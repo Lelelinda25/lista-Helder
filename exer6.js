45e635bca5f6f3e8b3fc61a5763e4c0727a8c59a
Lelelinda25 <leticia.souza10@estudante.ifms.edu.br>
2026-06-01 14:19:45 -0400

    exer6

---
+++ exer6.js
@@ -0,0 +1,25 @@
+class pilha {
+   #itens = [];
+  
+   empilhar(item) {
+     this.#itens.push(item);
+   }
+
+    desempilhar() {
+        return this.#itens.pop();
+    }
+
+    topo() {
+        return this.#itens[this.#itens.length - 1]; 
+     }
+
+     estaVazia() {
+        return this.#itens.length === 0;
+     }
+
+     get tamanho() {
+        return this.#itens.length;
+     }
+
+
+    }
