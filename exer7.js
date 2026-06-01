a04e344e7dca7ef8a5f0ea10f16ad46ed20825b5
Lelelinda25 <leticia.souza10@estudante.ifms.edu.br>
2026-06-01 14:28:52 -0400

    exer7

---
+++ exer7.js
@@ -0,0 +1,26 @@
+class Animal {
+    constructor(nome, som) {
+        this.nome = nome;
+        this.som = som;
+    }
+
+    emitirSom() {
+        return `${this.nome} diz: ${this.som}`;
+    }
+}
+
+class Cachorro extends Animal {
+    constructor(nome, som, raca) {
+        super(nome, som);
+        this.raca = raca;
+    }
+
+    emitirSom() {
+        return `O cachorro ${this.nome} diz: ${this.som}`;
+    }
+}
+
+
+const cachorro = new Cachorro("Rex", "Au Au", "Labrador");
+
+console.log(cachorro.emitirSom());
